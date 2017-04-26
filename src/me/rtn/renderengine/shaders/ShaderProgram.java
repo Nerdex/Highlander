8e22818a9c0f566c269326b9632271b8e45324d7
RapidTheNerd <george.marr.cobc@gmail.com>
2017-04-26 22:33:10 +0100

    constructor + clean up

--- src/me/rtn/renderengine/shaders/ShaderProgram.java
+++ src/me/rtn/renderengine/shaders/ShaderProgram.java
@@ -4,7 +4,6 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -18,6 +17,12 @@ public abstract class ShaderProgram {
     private int vertexShaderID;
     private int fragmentShaderID;
 
+
+    public ShaderProgram(String vertexFile, String fragmentFile){
+        vertexShaderID = loadShader(vertexFile, GL20.GL_VERTEX_SHADER);
+        fragmentShaderID = loadShader(fragmentFile, GL20.GL_FRAGMENT_SHADER);
+    }
+
     //need this to load and read the shader files
     private static int loadShader(String file, int type){
         StringBuilder source = new StringBuilder();
@@ -29,8 +34,6 @@ public abstract class ShaderProgram {
             }
             reader.close();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,7 +41,7 @@ public abstract class ShaderProgram {
         int shaderID = GL20.glCreateShader(type);
         GL20.glShaderSource(shaderID, ByteBuffer.allocateDirect(type));
         GL20.glCompileShader(shaderID);
-        if(GL20.glGetShader(shaderID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE){
+        if(GL20.glGetShaderi(shaderID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE){
             System.out.println(GL20.glGetShaderInfoLog(shaderID, 500));
             System.out.println("Could not compile shader, abandoning thread");
             System.exit(-1);
