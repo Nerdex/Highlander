828bd6b5cf692d97b183d909f9a802fe0191cd94
RapidTheNerd <george.marr.cobc@gmail.com>
2017-04-28 00:40:37 +0100

    uhhhh java why u so stooopid

--- src/me/rtn/renderengine/Renderer.java
+++ src/me/rtn/renderengine/Renderer.java
@@ -4,6 +4,7 @@ import com.sun.org.glassfish.gmbal.ManagedData;
 import me.rtn.renderengine.models.RawModel;
 import me.rtn.renderengine.models.TexturedModel;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
@@ -20,14 +21,19 @@ public class Renderer {
     }
 
     //rendering the raw model
-    public void render(TexturedModel tModel){
-        RawModel model = tModel.getRawModel();
+    public void render(TexturedModel texturedModelModel){
 
+
+        RawModel model = texturedModelModel.getRawModel();
         GL30.glBindVertexArray(model.getVaoID());
         GL20.glEnableVertexAttribArray(0);
+        GL20.glEnableVertexAttribArray(1);
+        GL13.glActiveTexture(GL13.GL_TEXTURE0);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, texturedModelModel.getTexture().getTextureID());
         GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, model.getVertexCount());
         GL11.glDrawElements(GL11.GL_TRIANGLES, model.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
         GL20.glDisableVertexAttribArray(0);
+        GL20.glDisableVertexAttribArray(1);
         GL30.glBindVertexArray(0);
     }
 }
