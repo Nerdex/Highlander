bc43d3d92b062b96b22b66e25abf9eedcc2281ec
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-09 00:22:03 +0100

    changed all the copyright messages to be an OCD fuck

--- src/me/rtn/renderengine/models/RawModel.java
+++ src/me/rtn/renderengine/models/RawModel.java
@@ -1,7 +1,21 @@
 package me.rtn.renderengine.models;
 
-/**
- * Created by George on 26-Apr-17 on Apr at 2:01 AM.
+/*
+ * 3DGa,e
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class RawModel {
 
--- src/me/rtn/renderengine/shaders/StaticShader.java
+++ src/me/rtn/renderengine/shaders/StaticShader.java
@@ -7,8 +7,22 @@ import me.rtn.renderengine.utils.Maths;
 
 import javax.vecmath.Matrix4f;
 
-/**
- * Created by George on 26-Apr-17 on Apr at 10:38 PM.
+/*
+ * 3DGa,e
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class StaticShader extends ShaderProgram {
 
