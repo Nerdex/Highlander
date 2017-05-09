1ef232277ea00f8c2a9fbea9ea273c57e709ec8a
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-09 21:49:18 +0100

    vertex is fun...not

---
+++ src/me/rtn/renderengine/objConverter/Vertex.java
@@ -0,0 +1,33 @@
+package me.rtn.renderengine.objConverter;/*
+ * 3DGa,e
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import javax.vecmath.Vector3f;
+
+public class Vertex {
+
+    private final int NO_INDEX = -1;
+
+    private Vector3f position;
+    private int textureIndex = NO_INDEX;
+    private int normalIndex = NO_INDEX;
+    private Vertex duplicateVertex = null;
+    private int index;
+    private float length;
+
+    
+}
