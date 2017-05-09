c086fcde46851c6b8abb66e3fd19c7d07491ef8c
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-09 21:58:10 +0100

    loading files is fun

---
+++ src/me/rtn/renderengine/objConverter/OBJFileLoader.java
@@ -0,0 +1,47 @@
+package me.rtn.renderengine.objConverter;/*
+ * 3DGa,e
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import javax.vecmath.Vector2f;
+import javax.vecmath.Vector3f;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OBJFileLoader {
+
+    private static final String RES_LOC = "res/";
+
+    public static ModelData loadOBJ(String objFileName){
+        FileReader reader = null;
+        File objFile = new File(RES_LOC + objFileName + ".obj");
+        try{
+            reader = new FileReader(objFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader bReader = new BufferedReader(reader);
+        String line;
+        List<Vertex> vertices = new ArrayList<Vertex>();
+        List<Vector2f> textures = new ArrayList<Vector2f>();
+        List<Vector3f> normals = new ArrayList<Vector3f>();
+        List<Integer> indices = new ArrayList<Integer>();
+    }
+}
