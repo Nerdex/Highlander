6a0bd015f69c3dca807be0b376d14e560d6b5833
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-10 20:09:15 +0100

    added more textures

--- out/production/3DGame/executors/execute.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set CLASSPATH=.
-set CLASSPATH=%CLASSPATH%;path/to/needed/jars/my.jar
-%JAVA_HOME%\bin\jar -Xms128 -Xmx384m -Xnoclassgc //classname
-
-#Will change when the game is complete
