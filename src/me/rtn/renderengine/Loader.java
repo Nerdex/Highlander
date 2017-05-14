4692302f086a93ba1a01c453b5793dc792b3f446
RapidTheNerd <george.marr.cobc@gmail.com>
2017-05-14 20:30:22 +0100

    adding lod bias

--- src/me/rtn/renderengine/Loader.java
+++ src/me/rtn/renderengine/Loader.java
@@ -3,10 +3,7 @@ package me.rtn.renderengine;
 
 import me.rtn.renderengine.models.RawModel;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.*;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
@@ -60,6 +57,7 @@ public class Loader {
             texture = TextureLoader.getTexture("PNG", new FileInputStream("res/"+fileName+".png"));
             GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+            GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_MAX_TEXTURE_LOD_BIAS, -1);
         } catch (IOException e) {
             e.printStackTrace();
         }
